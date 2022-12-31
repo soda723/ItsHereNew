7f167b73279732e98d3572092cc083c0abb5fd50
Jiwon <26162616@naver.com>
2022-12-31 17:33:05 +0900

    zxing barcode ok

--- app/src/main/java/com/capstone/itshere/membership/MBSitemAdapter.java
+++ app/src/main/java/com/capstone/itshere/membership/MBSitemAdapter.java
@@ -1,10 +1,13 @@
 package com.capstone.itshere.membership;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -13,12 +16,17 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.capstone.itshere.R;
 import com.google.api.Distribution;
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.WriterException;
+import com.journeyapps.barcodescanner.BarcodeEncoder;
 
 import java.util.ArrayList;
 
 public class MBSitemAdapter extends RecyclerView.Adapter<MBSitemAdapter.MBSItemHolder> {
     private ArrayList<MBSitem> arrayList;
     private Context context;
+    private MBSitem myitem;
+    private ImageView barcode_image;
 
     public MBSitemAdapter(ArrayList<MBSitem> arrayList, Context context) {
         this.arrayList = arrayList;
@@ -30,15 +38,32 @@ public class MBSitemAdapter extends RecyclerView.Adapter<MBSitemAdapter.MBSItemH
     @Override
     public MBSItemHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.membership_item, parent, false);
-        MBSItemHolder holder = new MBSItemHolder(view);
+        //MBSItemHolder holder = new MBSItemHolder(view);
+        MBSItemHolder holder = null;
+        try {
+            holder = new MBSItemHolder(view);
+        } catch (WriterException e) {
+            e.printStackTrace();
+            Log.e("홀더 널 : ", e+" ");
+        }
         return holder;
     }
 
     @Override
     public void onBindViewHolder(@NonNull MBSitemAdapter.MBSItemHolder holder, int position) {
-        MBSitem myitem = arrayList.get(position);
+        myitem = arrayList.get(position);
         holder.tv_mbs_contents.setText(myitem.getContents());
         holder.ly_color.setBackgroundColor(myitem.getColor());
+        holder.barcode_text.setText(myitem.getBarcode());
+
+        try {
+            BarcodeEncoder barcodeEncoder = new BarcodeEncoder();
+            String contents = myitem.getBarcode().replace(" ","");
+            Bitmap bitmap = barcodeEncoder.encodeBitmap(contents, BarcodeFormat.CODE_128, 300, 50);
+            barcode_image.setImageBitmap(bitmap);
+        } catch(Exception e) {
+            barcode_image.setImageResource(R.drawable.logo);
+        }
 
     }
 
@@ -48,21 +73,23 @@ public class MBSitemAdapter extends RecyclerView.Adapter<MBSitemAdapter.MBSItemH
     }
 
     public class MBSItemHolder extends RecyclerView.ViewHolder{
-        TextView tv_mbs_contents;
+        TextView tv_mbs_contents, barcode_text;
         ImageButton btn_delete, btn_pencil;
         LinearLayout ly_color;
 
-        public MBSItemHolder(@NonNull View itemview){
+        public MBSItemHolder(@NonNull View itemview) throws WriterException {
             super(itemview);
             this.tv_mbs_contents = itemview.findViewById(R.id.mbsitem_name);
             this.btn_delete = itemview.findViewById(R.id.mbsitem_btn_delete);
             this.btn_pencil = itemview.findViewById(R.id.mbsitem_btn_pencil);
             this.ly_color = itemview.findViewById(R.id.mbsitem_ly_color);
+            this.barcode_text = itemview.findViewById(R.id.barcode_text);
+            barcode_image = itemview.findViewById(R.id.barcode_image);
 
             btn_delete.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
-
+                    
                 }
             });
 
